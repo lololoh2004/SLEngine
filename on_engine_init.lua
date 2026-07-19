38ab40640ba40dc3e13138b8af360651ad0f1904
lololoh2004 <kofemolkahoho@gmail.com>
2026-07-19 14:14:59 +0300

    Changed autorun idea and created a mini-launcher.
    Right now, the "bp" project folder should contain ONLY one file "/lua/on_init.lua" which is responsible for loading the other autorun files (previously, I had intended for all files in "/lua/autorun/" to execute sequentially). Also the engine should present all projects to you upon startup (it scans the entire "/<engine>/bp_mods/" folder and in the event of a crash will report the issue and continue running)

--- on_engine_init.lua
+++ on_engine_init.lua
@@ -1,3 +1,7 @@
+---------------------------------------------
+-- You can leave small on-init things here --
+---------------------------------------------
+
 --utils.system_launch("notepad.exe", "")
 console.pro_print('bold', 'success', 'hello in green', 123, true)
 --
@@ -8,19 +12,30 @@ console.pro_print('bold', 'success', 'hello in green', 123, true)
 --}
 --console.pick("hello its pick", options)
 
--- You can leave small on-init things here
 
-engine.add_hook("on_render_ui", "my_menu", function()
+local bp_files = utils.dir_to_tab("./bp_mods")
+--for i = 1, #files do
+--    print("File [" .. i .. "]: " .. files[i])
+--end
+
+engine.add_hook("on_render_ui", "bp_choice", function()
     sgui.begin_window("BP TO RUN..")
-        if sgui.add_button("Run test click") then
-            console.msg("CLICK!\n")
+    for i = 1, #bp_files do
+        local bp_name = bp_files[i]
+        if sgui.add_button(bp_name) then
+            console.msg("[LUA] INFO : You launch " .. bp_name .. "\n   Upon successful startup, the engine GUI will close..\n\n")
+            local success, error_msg = pcall(dofile, "./bp_mods/" .. bp_name .. "/lua/on_init.lua")
+            if not success then
+                console.msg("[LUA] ERROR : NO_INIT_FILE_IN_BP : " .. bp_name .. ":\n   " .. tostring(error_msg) .. "\n")
+            else
+                --console.msg("[LUA] INFO : Success !  : " .. bp_name .. " loaded correctly\n   Engine GUI will close" .. "\n")
+                engine.del_hook("on_render_ui", "bp_choice")
+            end
         end
+    end
     sgui.end_window()
 end)
 
+--engine.del_hook("on_render_ui", "bp_choice")
 
-local files = utils.dir_to_tab(".")
 
-for i = 1, #files do
-    print("File [" .. i .. "]: " .. files[i])
-end
